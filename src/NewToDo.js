6fc0f3b9d8d642d3979dc1ce6ebbc9433451b12f
Sergey <sergeishmaro@gmail.com>
2017-09-21 00:31:35 +0300

    refactoring: NewToDo.js and ToDoTable.js

--- src/NewToDo.js
+++ src/NewToDo.js
@@ -3,25 +3,22 @@ import './NewToDo.css';
 
 class NewToDo extends Component {
 
-	render() {
-		return(
+  render() {
+    return(
       <div className="addTask">
-          <input type="text" className="addInput" 
-          			 onChange={this.props.inputOnChange} 
-          			 value={this.props.inputValue}
-								 onKeyDown={this.props.waitEnter}
-								 autoFocus/>
-          <button className="btn newBtn" 
-									onClick={this.props.newTask}>
-						Add new task
-					</button>
-					<button className="btn clearBtn" 
-									onClick={this.props.clearList}>
-						Clear task List
-					</button>
+        <input 
+          type="text" 
+          className="addInput" 
+          onChange={this.props.inputOnChange} 
+          value={this.props.inputValue}
+          onKeyDown={this.props.waitEnter}
+          autoFocus
+        />
+        <button className="btn newBtn" onClick={this.props.newTask}>Add new task</button>
+        <button className="btn clearBtn" onClick={this.props.clearList}>Clear task List</button>
       </div>
     );
-	}
+  };
 }
 
 export default NewToDo;
--- src/ToDoTable.js
+++ src/ToDoTable.js
@@ -3,47 +3,50 @@ import './ToDoTable.css';
 
 class ToDoTable extends Component {
 
-	toJSXArray = () => {
-		if(this.props.state.tasks.length === 0)
-		{
-			return(
-				<li>
-					<p>There are no tasks yet</p>
-				</li>
-			);
-		}
-		return (
-			this.props.state.tasks.map((item, index) => {
-        const checkId = "check" + index;
-				if(!item.checked) {
-					return(
-						<li key={index}>
-							<input type="checkbox" id={checkId} checked={item.checked} 
-							   		 onChange={this.props.change(index)}/>
-							<label htmlFor={checkId}>{item.task}</label>
-						</li>
-					);
-				} else {
-					return(
-						<li key={index} className="crossed">
-							<input type="checkbox" id={checkId} checked={item.checked}/>
-							<label htmlFor={checkId}>{item.task}</label>
-						</li>
-					)
-				}
-			})
-		);
-	}
-
-	render() {
-		return(
-		  <ul className="toDoList">
-     		{
-     			this.toJSXArray()
-     		}
-     	</ul>
+  emptyRender = () => {
+    return (
+      <ul className="toDoList">
+        <li>
+          <p>There are no tasks yet</p>
+        </li>
+      </ul>
     );
-	}
+  }
+
+  normalRender = () => {
+    const jsxarr = this.props.state.tasks.map((item, index) => {
+      const checkId = "check" + index;
+      if (!item.checked) {
+        return (
+          <li key={index}>
+            <input type="checkbox" id={checkId} checked={item.checked} 
+                    onChange={this.props.change(index)}/>
+            <label htmlFor={checkId}>{item.task}</label>
+          </li>
+        );
+      } else {
+        return (
+          <li key={index} className="crossed">
+            <input type="checkbox" id={checkId} checked={item.checked}/>
+            <label htmlFor={checkId}>{item.task}</label>
+          </li>
+        )
+      }
+    })
+    return (
+      <ul className="toDoList">
+        {jsxarr}
+      </ul>
+    );
+  }
+
+  render = () => {
+    if (this.props.state.tasks.length === 0) {
+      return this.emptyRender();
+    } else {
+      return this.normalRender();
+    }
+  }
 }
 
 export default ToDoTable;
